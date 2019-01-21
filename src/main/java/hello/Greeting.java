024a704cb09ade47e24d4769fec615969f31ffc4
Narwanto <narwanto@nostratech.com>
2019-01-21 17:10:20 +0700

    fixing bug task 1

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -2,21 +2,15 @@ package hello;
 
 public class Greeting {
 
-    private final long id;
     private final String content;
     private final String gender;
 
-    public Greeting(long id, String content, String gender) {
-        this.id = id;
+    public Greeting(String content, String gender) {
         this.content = content;
         this.gender = gender;
     }
 
     public String getGender() {return gender;}
-
-    public long getId() {
-        return id;
-    }
 
     public String getContent() {
         return content;
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -11,9 +11,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping("/employee")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name,@RequestParam(value="gender", defaultValue="Male") String gender) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name),gender);
+        return new Greeting(String.format(template, name),gender);
     }
 }
