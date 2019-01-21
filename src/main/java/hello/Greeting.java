c5110fe94fa2cebf416311ab5b78274de17abc2e
Narwanto <narwanto@nostratech.com>
2019-01-21 15:06:52 +0700

    add gender in response greeting

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -4,11 +4,15 @@ public class Greeting {
 
     private final long id;
     private final String content;
+    private final String gender;
 
-    public Greeting(long id, String content) {
+    public Greeting(long id, String content, String gender) {
         this.id = id;
         this.content = content;
+        this.gender = gender;
     }
+
+    public String getGender() {return gender;}
 
     public long getId() {
         return id;
