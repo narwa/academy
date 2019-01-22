f9380765d10bba068583f155ea704f7fc1cb1a3f
Narwanto <narwanto@nostratech.com>
2019-01-22 12:13:22 +0700

    conflict

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+
+        System.out.println("ini contoh conflict");
+
         SpringApplication.run(Application.class, args);
     }
 }
